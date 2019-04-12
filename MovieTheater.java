017b4450b1d07dd608e02400ec4105dee4b95805
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2019-04-12 16:32:09 -0400

    Update MovieTheater.java

--- MovieTheater.java
+++ MovieTheater.java
@@ -2,7 +2,27 @@
 public class MovieTheater {
 
 	public MovieTheater(double ticketPrice, Theater dumbo, Theater shazam) {
-		
+
 	}
 
+	public boolean hasCustomer(String customerName) {
+		if(customerName.equals("yes")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void addToLine(Customer customer, boolean hasChild) {
+
+		if(hasChild) {
+			System.out.println("Added with Express");
+		} else {
+			System.out.println("Added with in RegLine");
+		}
+	}
+	
+	
+	
 }
+
