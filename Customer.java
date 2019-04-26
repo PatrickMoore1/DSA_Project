fe54aa2683155a29f8397eb0d52316e7953d0642
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2019-04-25 22:05:39 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -3,15 +3,13 @@ public class Customer {
 	private int size = 0;
 	private String name = "";
 	boolean watchDumbo;
-	boolean watchShazam;
-	
-	
-	public Customer(int size, String name, boolean watchDumbo, boolean watchShazam) {
+
+
+	public Customer(int size, String name, boolean watchDumbo) {
 		super();
 		this.size = size;
 		this.name = name;
 		this.watchDumbo = watchDumbo;
-		this.watchShazam = watchShazam;
 	}
 
 
@@ -28,30 +26,8 @@ public class Customer {
 	public String getName() {
 		return name;
 	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public boolean isWatchDumbo() {
+	
+	public boolean getWatchDumbo() {
 		return watchDumbo;
 	}
-	
-	public boolean isWatchShazam() {
-		return watchShazam;
-	}
-
-
-	public void setWatchDumbo(boolean watchDumbo) {
-		this.watchDumbo = watchDumbo;
-	}
-
-
-	public void setWatchShazam(boolean watchShazam) {
-		this.watchShazam = watchShazam;
-	}
-	
-	
 }
