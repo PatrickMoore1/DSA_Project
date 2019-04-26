581c0a6cc838d8811fb4c5f28daa21b0f3edfd7a
PennNguyen <34245455+PennNguyen@users.noreply.github.com>
2019-04-25 21:05:40 -0400

    Update Theater.java

--- Theater.java
+++ Theater.java
@@ -1,40 +1,62 @@
 
 public class Theater  {
-	
+
 	private int rows = 0;
 	private int seatsPerRow = 0;
+	private int totalSeats;
+	private int filledSeats = 0;
+	private int currentSeat = 0;
 	private MyListReferenceBased<Customer> seats = new MyListReferenceBased<Customer>(); 
-	
-	
+
+
 	public Theater(int rows, int seatsPerRow) {  //User will be prompted how many rows and how many seats per row
 		super();
 		this.rows = rows;
 		this.seatsPerRow = seatsPerRow;
+		totalSeats = rows*seatsPerRow;
 	}
 
 	public int getRows() {
 		return rows;
-	}
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+	}  
 
 	public int getSeatsPerRow() {
 		return seatsPerRow;
 	}
 
-	public void setSeats(int seatsPerRow) {
-		this.seatsPerRow = seatsPerRow;
+	public void fillSeat(Customer customer) {
+		int numCust = customer.getSize();
+		if((filledSeats + numCust) < totalSeats) {
+			for(int i = 0; i < numCust; i++) {
+				seats.add(currentSeat, customer);
+				currentSeat++;
+				filledSeats++;
+			}
+		}
+		else {
+			System.out.println("No more seats");
+		}
 	}
 
-	public MyListReferenceBased<Customer> seats() {
-		return seats;
+	public boolean customerLeave(String custName) {
+		boolean customerFound = false;
+		int amountRemoved = 0;
+		Node<Customer> searchCust = seats.getHead();
+		for(int i = 0; i < totalSeats; i++) {
+			if(!searchCust.getItem().getName().equalsIgnoreCase(custName)) {
+				searchCust = searchCust.getNext();
+			}
+			else {
+				 amountRemoved = searchCust.getItem().getSize();
+				searchCust.setItem(null);
+				customerFound = true;
+			}
+		}
+		filledSeats -= amountRemoved;
+		return customerFound;
 	}
 	
-	
-	
-	
-	
-
+	public boolean isMovieFull() {
+		return filledSeats == totalSeats;
+	}
 }
