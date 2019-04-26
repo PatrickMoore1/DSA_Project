dc173b972b14d23e161c4d31a3a8fc342190752d
PatrickMoore1 <35699967+PatrickMoore1@users.noreply.github.com>
2019-04-26 16:45:53 -0400

    Delete QueueException.java

--- QueueException.java
+++
@@ -1,8 +0,0 @@
-
-@SuppressWarnings("serial")
-public class QueueException extends RuntimeException {
-
-  public QueueException(String s) {
-    super(s);
-  }  // end constructor
-}  // end QueueException
