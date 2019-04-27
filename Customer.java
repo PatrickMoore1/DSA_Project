26af16ef5ab0254843be7da23df6b2075878fdeb
PennNguyen <34245455+PennNguyen@users.noreply.github.com>
2019-04-26 21:20:58 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,10 +1,24 @@
 
+/**
+ * This is the customer which will handle the group name, the size of the group and what movie 
+ * they are watching
+ * 
+ * 
+ * @author Patrick Moore & Kevin Nguyen
+ *
+ */
 public class Customer {
 	private int size = 0;
 	private String name = "";
 	boolean watchDumbo;
 
-
+	/**
+	 * Takes in the customer information
+	 * 
+	 * @param size			The size of the party
+	 * @param name			The name of the group
+	 * @param watchDumbo	If they are watching dumbo or not
+	 */
 	public Customer(int size, String name, boolean watchDumbo) {
 		super();
 		this.size = size;
@@ -12,19 +26,37 @@ public class Customer {
 		this.watchDumbo = watchDumbo;
 	}
 
-
+	/**
+	 * Gives back the size of the group
+	 * 
+	 * @return	int
+	 */
 	public int getSize() {
 		return size;
 	}
-
+	
+	/**
+	 * Gives back the name of the group
+	 * 
+	 * @return String
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Checks if group is watching dumbo
+	 * @return	watchDumbo
+	 */
 	public boolean getWatchDumbo() {
 		return watchDumbo;
 	}
 	
+	/**
+	 * Sets the group to not watch dumbo
+	 * 
+	 * @param watchDumbo
+	 */
 	public void setWatchDumbo(boolean watchDumbo) {
 		this.watchDumbo = watchDumbo;
 	}
